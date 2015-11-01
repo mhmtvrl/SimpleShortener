0b4474792b5af490ee2a39e5657735da02deb789
mhmtvrl <mhmtvrl1731@gmail.com>
2015-11-01 01:06:32 -0500

    DB connection function has been added

--- Shortener.php
+++ Shortener.php
@@ -15,8 +15,28 @@ function getRandomString($length)
     return $randomString;
 }
 
-echo $_POST['url'];
+function connectToDatabase()
+{
+    global $configuration;
+
+    $serverName = $configuration['serverName'];
+    $userName = $configuration['userName'];
+    $password = $configuration['password'];
+    $databaseName = $configuration['databaseName'];
+
+    try {
+        $conn = new PDO("mysql:host=$serverName;dbname=$databaseName", $userName, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch (PDPException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+
+//echo $_POST['url'];
 echo '<br>';
 echo getRandomString(5);
 echo '<br>';
 echo 'userName ' . $configuration['userName'];
+echo '<br>';
+connectToDatabase();
